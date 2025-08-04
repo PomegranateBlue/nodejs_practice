8d3cac62ccc989c2ef9cfccdfbed790fa6b6b357
bluepome98 <wpdn1998@gmail.com>
2025-08-04 14:50:08 +0900

    입력 폼 생성 후 리디렉션 처리 구현

--- app.js
+++ app.js
@@ -3,17 +3,31 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("hello");
-  next(); // 요청 처리를 계속 이어가게 함,next() 없이 응답 호출하면 미들웨어 도달 불가
+
+
+app.use("/add-product", (req, res, next) => {
+  console.log("another middleware");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>add Product</button></form>"
+  );
 });
 
-app.use((req, res, next) => {
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+/* 위의 /product 경로는 마지막 app.use 전에 배치만 하면, 어느 위치에 있던지 상관없다*/
+
+/**요청은 위에서 아래로 흘러가는 방식이며, next를 호출하지 않으면 다음 미들웨어로 넘어가지 않음 */
+
+app.use("/", (req, res, next) => {
   console.log("hello2");
   res.send("<h1>This is from Hello2</h1>"); //기본 응답은 text/html
 });
 
 // Express 앱을 http 서버에 연결해야 함
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(3000);
+// server.listen(3000);
+app.listen(3000); // 위에 존재하는 코드를 한 줄로 변경
